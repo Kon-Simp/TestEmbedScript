293a9267feb6737dae78cdadae887a9ae905866a
Kon-Simp <100676120+Kon-Simp@users.noreply.github.com>
2023-03-30 12:43:54 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import EmbedSurvey from "./embedsurvey/EmbedSurvey";
+
+export default function App() {
+  return (
+    <div className="App">
+      <EmbedSurvey />
+    </div>
+  );
+}
---
+++ src/embedsurvey/EmbedSurvey.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const EmbedSurvey = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        backgroundColor: "red",
+        height: "150px",
+        textAlign: "center"
+      }}
+    >
+      Start of embed survey
+    </div>
+  );
+};
+
+export default EmbedSurvey;
